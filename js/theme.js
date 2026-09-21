a8c966774c1781cecd76991d17bddd4475d4ffe2
YauhenBuinitski <bujnickijauhen@gmail.com>
2026-09-22 01:24:04 +0200

    feat: complete layout with dark theme toggle

---
+++ js/theme.js
@@ -0,0 +1,14 @@
+const themeToggle = document.getElementById('theme-toggle');
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+}
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+if (document.body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});    
